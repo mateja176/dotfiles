d04b13bf64800ca3f18bdfa0cfd8ed15bae31ad4
Mateja <mateja176@gmail.com>
2021-02-01 20:56:36 +0100

    adds docker-compose

--- curl_install.sh
+++ curl_install.sh
@@ -12,3 +12,7 @@ curl -fsSL https://deno.land/x/install/install.sh | bash
 
 # graphiql
 curl -o ~/bin/graphiql.AppImage https://github.com/skevy/graphiql-app/releases/download/v0.7.2/graphiql-app-0.7.2-x86_64.AppImage
+
+# docker-compose
+sudo curl -Lo -/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)"
+sudo chmod +x /usr/local/bin/docker-compose
