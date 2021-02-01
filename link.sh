87a159443de4a63ec6243283cb87d30615564f9c
Mateja <mateja176@gmail.com>
2021-02-01 19:10:37 +0100

    adds autostart

--- link.sh
+++ link.sh
@@ -2,3 +2,5 @@ ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/config.fish ~/.config/fish/config.fish
 ln -s ~/dotfiles/.eslintrc.json ~/.eslintrc.json
 ln -s ~/dotfiles/.prettierrc.json ~/.prettierrc.json
+
+ln -s ~/dotfiles/autostart ~/.config/autostart
